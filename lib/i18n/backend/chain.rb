63b10cee29a5463abf6b6b60f52ff45650586955
Matt <matt.beedle@1000jobboersen.de>
2010-06-08 17:29:58 +0200

    Chain backend will now return {} if {} supplied as default

--- lib/i18n/backend/chain.rb
+++ lib/i18n/backend/chain.rb
@@ -53,7 +53,7 @@ module I18n
             rescue MissingTranslationData
             end
           end
-          return namespace unless namespace.empty?
+          return namespace unless namespace.empty? and namespace != default
           raise(I18n::MissingTranslationData.new(locale, key, options))
         end
 
